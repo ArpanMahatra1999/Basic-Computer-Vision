78b73d51c7f0f0a7533423f377548574a9fad14c
Arpan Mahatra <47106999+ArpanMahatra1999@users.noreply.github.com>
2020-04-10 21:32:01 +0545

    Update resizeImage.py

--- resizeImage.py
+++ resizeImage.py
@@ -1,7 +1,8 @@
 import cv2
 
 img = cv2.imread("duck.jpg", 0)
-resize_img = cv2.resize(img, (650, 500))
+# resize_img = cv2.resize(img, (650, 500))
+resize_img = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
 cv2.imshow("Duck", resize_img)
 
 # Wait for pressing key to close window
